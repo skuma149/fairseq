57ae3bb11d7a51c00e8e4de56c6de213c6f89850
suryanshu kumar <skuma149@asu.edu>
2020-04-12 11:58:43 -0700

    adding cosmos task

--- evaluate.py
+++ evaluate.py
@@ -3,11 +3,11 @@ import torch
 from fairseq.models.roberta import RobertaModel
 from examples.roberta import commonsense_qa  # load the Commonsense QA task
 print("all fine till here")
-roberta = RobertaModel.from_pretrained(model_name_or_path='roberta.base',data_name_or_path='./data/CommonsenseQ')
+roberta = RobertaModel.from_pretrained(model_name_or_path='roberta.large',data_name_or_path='./data/CommonsenseQ')
 roberta.eval()  # disable dropout
 roberta.cuda()  # use the GPU (optional)
 nsamples, ncorrect = 0, 0
-with open('data/CommonsenseQA/valid.jsonl') as h:
+with open('data/cosmosQA/valid') as h:
     for line in h:
         example = json.loads(line)
         scores = []
---
+++ examples/roberta/cosmos_qa/__init__.py
@@ -0,0 +1,1 @@
+from . import cosmos_qa_task
